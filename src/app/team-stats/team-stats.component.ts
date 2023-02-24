d14fd30ff087234168f07da413face7c6aba7b78
s.marie <silvere@live.fr>
2023-02-24 20:04:41 +0100

    step 3

--- src/app/team-stats/team-stats.component.ts
+++ src/app/team-stats/team-stats.component.ts
@@ -1,30 +1,42 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Observable, tap} from 'rxjs';
+import {BehaviorSubject, map, Observable, switchMap} from 'rxjs';
 import {NbaService} from '../nba.service';
 import {Game, Stats, Team} from '../data.models';
-import {ActivatedRoute, ActivationEnd, Router} from "@angular/router";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-team-stats',
   templateUrl: './team-stats.component.html',
   styleUrls: ['./team-stats.component.css']
 })
-export class TeamStatsComponent implements OnInit {
+export class TeamStatsComponent {
 
   @Input()
   team!: Team;
 
-  games$!: Observable<Game[]>;
-  stats!: Stats;
-  constructor(private nbaService: NbaService, private router: Router) { }
+  availableOptions = [6, 12, 20]
+  lastResultQuery = new BehaviorSubject<number>(this.availableOptions[0])
 
-  ngOnInit(): void {
-    this.games$ = this.nbaService.getLastResults(this.team, 12).pipe(
-      tap(games =>  this.stats = this.nbaService.getStatsFromGames(games, this.team))
+
+  stats = this.lastResultQuery.pipe(
+    switchMap(
+      (numberOfDay: number) => this.nbaService.getLastResults(this.team, numberOfDay)
+    ),
+    map(
+      (it) => this.nbaService.getStatsFromGames(it, this.team)
     )
+  );
+
+
+  constructor(private nbaService: NbaService, private router: Router) {
   }
 
+
   deleteClicked() {
-    this.router.navigate([{ outlets: { popup: ['delete', this.team.id] } }])
+    this.router.navigate([{outlets: {popup: ['delete', this.team.id]}}])
+  }
+
+  onPeriodChange(duration: string) {
+    this.lastResultQuery.next(Number.parseInt(duration))
   }
 }
